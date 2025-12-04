ec3138efefdc0c03554ce8dd7cc5298973062cbc
Papemomath <papemomathdiba@gmail.com>
2025-12-04 14:01:33 +0000

    init project

--- src/api/src/main/java/com/mariadb/todo/domain/Task.java
+++ src/api/src/main/java/com/mariadb/todo/domain/Task.java
@@ -1,16 +1,38 @@
 package com.mariadb.todo.domain;
 
-import javax.persistence.*;
 
-import lombok.Data;
+import jakarta.persistence.*;
 
-@Data
 @Entity
 @Table(name = "tasks")
 public class Task {
-    @Id 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String description;
     private Boolean completed = false;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
 }
